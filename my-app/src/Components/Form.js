88dcc9467709590831c7a258eb0dfc97efd27b87
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-11-14 16:20:24 +0000

    added From component and validation schema

--- my-app/src/Components/Form.js
+++ my-app/src/Components/Form.js
@@ -1,1 +1,25 @@
+import React from "react";
 import axios from "axios";
+
+import { Formik, Form as FormikForm, Field, ErrorMessage } from "formik";
+
+const Form = ({ initialValues, onSubmit, validationSchema }) => {
+  return (
+    <Formik
+      onSubmit={onSubmit}
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      render={props => (
+        <FormikForm>
+          <Field type="text" name="name" placeholder="Name" />
+          <Field type="email" name="email" placeholder="Email" />
+          <Field type="Password" name="Password" placeholder="password" />
+          <Field type="checkbox" name="" placeholder="Terms of Service" />
+          <button>Submit!</button>
+        </FormikForm>
+      )}
+    />
+  );
+};
+
+export default Form;
