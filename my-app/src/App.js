f3f1b43588dcc090d83ef75fa66f6c8240647b50
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-11-14 16:46:57 +0000

    added form validation, post request and error messages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import * as yup from "yup";
 import Form from "./Components/Form";
+import axios from "axios";
 
 const initialFormState = {
   name: "",
@@ -11,18 +12,31 @@ const initialFormState = {
   terms: ""
 };
 
-const validationSchema = yup.object.shape({
-  name: yup.string().required(),
+const validationSchema = yup.object().shape({
+  name: yup.string().required("Must type a name"),
   email: yup
     .string()
     .email()
-    .required(),
-  password: yup.string().required(),
-  terms: yup.boolean().required()
+    .required("Must type an email"),
+  password: yup
+    .string()
+    .required("Must type a password with 6+ characters")
+    .length(6),
+  terms: yup.boolean().required("Must Accept Terms and Conditions")
 });
 
 function App() {
-  const addPerson = form => {};
+  const addPerson = form => {
+    axios
+      .post("https://reqres.in/api/users_")
+      .then(res => {
+        console.log(res);
+        form.resetForm();
+      })
+      .catch(error => {
+        console.error(error.message);
+      });
+  };
   return (
     <div className="App">
       <Form
--- my-app/src/Components/Form.js
+++ my-app/src/Components/Form.js
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 
 import { Formik, Form as FormikForm, Field, ErrorMessage } from "formik";
 
@@ -11,11 +10,23 @@ const Form = ({ initialValues, onSubmit, validationSchema }) => {
       validationSchema={validationSchema}
       render={props => (
         <FormikForm>
-          <Field type="text" name="name" placeholder="Name" />
-          <Field type="email" name="email" placeholder="Email" />
-          <Field type="Password" name="Password" placeholder="password" />
-          <Field type="checkbox" name="" placeholder="Terms of Service" />
-          <button>Submit!</button>
+          <label htmlFor="name">Name: </label>
+          <Field name="name" type="text" placeholder="Name" />
+          <ErrorMessage name="name" component="div" />
+          <br />
+          <label htmlFor="email">Email: </label>
+          <Field name="email" type="email" placeholder="Email" />
+          <ErrorMessage name="email" component="div" />
+          <br />
+          <label htmlFor="password">Password: </label>
+          <Field name="password" type="password" placeholder="Password" />
+          <ErrorMessage name="password" component="div" />
+          <br />
+          <label htmlFor="terms">Terms: </label>
+          <Field name="terms" type="checkbox" placeholder="Terms" value="1" />
+          <ErrorMessage name="terms" component="div" />
+          <br />
+          <button type="submit">Submit</button>
         </FormikForm>
       )}
     />
